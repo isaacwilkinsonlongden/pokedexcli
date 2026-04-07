0ad889e0162bb550aa087f00311dcba7a0f600cb
isaacwilkinsonlongden <isaacwilkinsonlongden@gmail.com>
2026-04-07 14:38:24 +0100

    added func cleanInput and unit tests

---
+++ repl_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "hello, world",
+			expected: []string{"hello,", "world"},
+		},
+		{
+			input:    "HELLO WORLD",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  ",
+			expected: []string{},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("lengths don't match: '%v' vs '%v'", actual, c.expected)
+			continue
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("cleanInput(%v) == %v, expected %v", c.input, actual, c.expected)
+			}
+		}
+	}
+}
