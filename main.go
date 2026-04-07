baf7511cb367a7da7613c76dbceaf265dda3f9e4
isaacwilkinsonlongden <isaacwilkinsonlongden@gmail.com>
2026-04-07 15:09:57 +0100

    refactor REPL

--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		userInput := scanner.Text()
-		cleanedInput := cleanInput(userInput)
-		firstWord := cleanedInput[0]
-
-		fmt.Printf("Your command was: %s\n", firstWord)
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,8 +1,30 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		userInput := scanner.Text()
+		words := cleanInput(userInput)
+		if len(words) == 0 {
+			continue
+		}
+
+		firstWord := words[0]
+
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
+}
 
 func cleanInput(text string) []string {
 	words := strings.Fields(strings.ToLower(text))
