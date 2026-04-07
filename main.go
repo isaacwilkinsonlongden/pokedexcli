e7f7739486d3303301998151986d6091b907811f
isaacwilkinsonlongden <isaacwilkinsonlongden@gmail.com>
2026-04-07 15:03:30 +0100

    added REPL system to main.go

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		userInput := scanner.Text()
+		cleanedInput := cleanInput(userInput)
+		firstWord := cleanedInput[0]
+
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
 }
